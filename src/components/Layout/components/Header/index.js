296d52f26d0bf6e288b107dfe5a390449b75937e
DaoDev <boynguyen1998@gmail.com>
2022-05-13 14:45:04 +0700

    Add layout default

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles)
 
 function Header(props) {
     return (
-        <div>
-            <h1>
-                <h1>Header</h1>
-            </h1>
-        </div>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+
+            </div>
+        </header>
     );
 }
 
