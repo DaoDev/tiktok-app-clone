d78ecdc1471648f69e0415f6edc1f983d8943dd3
Dao Nguyen (BnK - HN) <dao.nguyen@bnksolution.com>
2022-05-19 16:14:58 +0700

    Add list menu item

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,15 +2,32 @@ import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion, faCircleXmark, faEarthAsia, faEllipsisVertical, faKeyboard, faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import { Wrapper as PopperWrapper } from '../../Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '../../Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon:<FontAwesomeIcon icon={faEarthAsia}/>,
+        title: 'English'
+    },
+    {
+        icon:<FontAwesomeIcon icon={faCircleQuestion}/>,
+        title: 'Feedback and help',
+        to:'/feedback'
+    },
+    {
+        icon:<FontAwesomeIcon icon={faKeyboard}/>,
+        title: 'Keyboard shortcuts'
+    },
+]
 
 function Header(props) {
     const [serachResult, setSerachResult] = useState([]);
@@ -58,6 +75,11 @@ function Header(props) {
                     <Button primary rightIcon={<FontAwesomeIcon icon={faSignIn} />}>
                         Login
                     </Button>
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
